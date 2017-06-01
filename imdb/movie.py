1532db4bf11f54d6e27fd42dceb9c8b11f7b07e6
Mustafa Bilgic <mbilgic@iit.edu>
2017-06-01 13:41:54 -0500

    Working version of genres

--- imdb/movie.py
+++ imdb/movie.py
@@ -1,10 +1,11 @@
 class Movie(object):
-    def __init__(self, title, year, rating_distribution, num_ratings, mean_rating):
-        self.title = title
-        self.year = year
-        self.rating_distribution = rating_distribution
-        self.num_ratings = num_ratings
-        self.mean_rating = mean_rating
-    
-    def __repr__(self):
-        return self.title + "\t" + str(self.year) + "\t" + self.rating_distribution + "\t" + str(self.num_ratings) + "\t" + str(self.mean_rating)
+	def __init__(self, title, year, rating_distribution, num_ratings, mean_rating):
+		self.title = title
+		self.year = year
+		self.rating_distribution = rating_distribution
+		self.num_ratings = num_ratings
+		self.mean_rating = mean_rating
+		self.genre = None
+		
+	def __repr__(self):
+		return self.title + "\t" + str(self.year) + "\t" + self.rating_distribution + "\t" + str(self.num_ratings) + "\t" + str(self.mean_rating)
--- imdb/parse.py
+++ imdb/parse.py
@@ -5,13 +5,16 @@ def parse_imdb_data():
 	import sys
 	from movie import Movie
 
-	file_path = "../../imdb/ratings.list"
+	
 
 	episode_re = re.compile("\(\d\d\d\d\)\s\{") # FIX: this does not match if the year has nondigit characters
 	year_re = re.compile("\(\d\d\d\d")
 	
 	movies = []
-
+	
+	print("PARSING THE RATINGS FILE")
+	#RATINGS FILE
+	file_path = "../../imdb/ratings.list"
 	with open(file_path, 'r') as f:
 		l = None
 		while(l != "MOVIE RATINGS REPORT"):
@@ -28,6 +31,8 @@ def parse_imdb_data():
 		#    m = f.readline()
 			if m == "\n":
 				break
+			if m.find("(VG)") != -1: #skip video games
+				continue
 			m=m.strip()
 			try:
 				m_list=m.split()
@@ -41,8 +46,56 @@ def parse_imdb_data():
 					year = int(year_match.group()[1:])
 					movies.append(Movie(title, year, rating_distribution, num_ratings, mean_rating))
 			except:
+				pass
 				#e = sys.exc_info()[0]
 				#print( "<p>Error: %s</p>" % e )
-				print("Failed to parse movie %s " %m)
+				#print("Failed to parse movie %s " %m)
+	# GENRES FILE
+	print("PARSING THE GENRES FILE")
+	file_path = "../../imdb/genres.list"
+	with open(file_path, 'r') as f:
+		l = None
+		while(l != "8: THE GENRES LIST"):
+			l = f.readline().strip()
+		#print(l)    
+		# Dashes
+		f.readline()
+		# Empty
+		f.readline()
+		# Movies
+		genres = {}
+		for m in f:			
+		#for i in range(1000):
+			#m = f.readline()
+			if m == "\n":
+				break
+			if m.find("(VG)") != -1: #skip video games
+				continue
+			m=m.strip()
+			try:
+				m_list=m.split()
+				#print(m_list)
+				genre = m_list[-1]
+				if episode_re.search(m) is None: # if not episode                
+					year_match = year_re.search(m)
+					title = m[:year_match.start()-1]
+					year = year_match.group()[1:]
+					#movies.append(Movie(title, year, rating_distribution, num_ratings, mean_rating))
+					#print("%s \t %d \t %s" %(title, year, genre))
+					tandy = title+year
+					if tandy in genres: # FIX: TWO OR MORE TITLES IN SEPARATE YEARS, GETS MATCHED TO THE SAME GENRE
+						genres[tandy].append(genre)
+					else:
+						genres[tandy]=[genre]
+			except:
+				pass
+				#e = sys.exc_info()[0]
+				#print( "<p>Error: %s</p>" % e )
+				#print("Failed to parse movie %s " %m)
+	
+	for m in movies:
+		if m.title+str(m.year) in genres:			
+			genre = genres[m.title+str(m.year)]        
+			m.genre = genre
 	
 	return movies
